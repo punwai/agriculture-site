e70fcddac96eb1ab8b29a5c06f5633099d73940f
Suppakit Waiwitlikhit <punwaiw@gmail.com>
2020-08-27 09:03:14 +0700

    f

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Login from './components/Login/Login';
 import Basket from './components/Basket/Basket';
 import TimeShare from './components/TimeShare/TimeShare';
 import Order from './components/Order/Order';
-
+import RailinkLogo from './RaiLink.svg';
 import LoginCallback from './components/LoginCallback/LoginCallback';
 import { CookiesProvider } from 'react-cookie';
 import { instanceOf } from 'prop-types';
@@ -60,9 +60,9 @@ class App extends Component {
       <body>
         <Router>
           <div>
-              <Container>
-                <Navbar fill className="custom-navbar" variant="light">
-                  <Navbar.Brand href="/home">ShareDi</Navbar.Brand>
+                <Navbar fill className="custom-navbar shadow" variant="light">
+                  <Container>
+                  <Navbar.Brand href="/home"><img src={RailinkLogo} alt="React Logo" height="45"/></Navbar.Brand>
                   <Nav fill className="mr-auto">
                     <Nav.Link href="/about"><b>เกี่ยวกับ</b></Nav.Link>
                     <Nav.Link href="/features">ข่าวสาร</Nav.Link>
@@ -71,30 +71,29 @@ class App extends Component {
                   </Nav>
                   <Nav>
 
-                  <Nav.Link href="/basket"><Button variant="outline-success">
+                  <Nav.Link href="/basket"><Button variant="success">
                     <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cart-fill top-margin-minus" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                       <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
                     </svg>&nbsp;ตะกร้า</Button>
                   </Nav.Link>
 
                   <Nav.Link href="/login">
-                    <Button variant="outline-secondary">
+                    <Button variant="secondary">
                       <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-file-person top-margin-minus" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M4 1h8a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2zm0 1a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H4z"/>
                         <path d="M13.784 14c-.497-1.27-1.988-3-5.784-3s-5.287 1.73-5.784 3h11.568z"/>
                         <path fill-rule="evenodd" d="M8 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
                       </svg> &nbsp;
-                      <b>เข้าสู่ระบบ</b>
+                      เข้าสู่ระบบ
                     </Button>
                   </Nav.Link>
 
                   </Nav>
-                </Navbar>   
-              </Container>
+                </Container>
+              </Navbar>   
 
             {/* A <Switch> looks through its children <Route>s and
                 renders the first one that matches the current URL. */}
-            <Container>
               <Switch>
                 <Route path="/about">
                 </Route>
@@ -107,9 +106,6 @@ class App extends Component {
                 <Route path="/login">
                   <Login/>
                 </Route>
-                <Route path="/callback">
-                  <LoginCallback/>
-                </Route>
                 <Route path="/basket">
                   <Basket/>
                 </Route>
@@ -117,7 +113,6 @@ class App extends Component {
                   <Home />
                 </Route>
               </Switch>
-            </Container>
           </div>
         </Router>
           <script src="https://www.gstatic.com/firebasejs/7.18.0/firebase-app.js"></script>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,45 +1,74 @@
 import React, { Component } from 'react';
 import { Nav, Navbar, Form, FormControl, Button, Container, Row, Col, Card } from 'react-bootstrap';
 import './Home.scss';
+import Graphics1 from './graphics1.svg';
+import Graphics2 from './graphics2.svg';
+import Graphics3 from './graphics3.svg';
+import Graphics4 from './graphics4.svg';
 
 export default class Home extends Component {
     render() {
         return (
             <div>
-                <Container fluid className="home-container">
+                <div className="home-container">
                     <Row className="filler">
                     </Row>
                     <Row>
                         <Container>
-                        <Col className="homeText">อุปกรณ์เกษตกร <br/>ไทม์แชร์ 6 เดือน 4 ปี <br/> ราคาถูก. ซื้อง่าย. หมดกังวล. <br/>
-                            <Button className="home-button">
-                                เริ่มต้นใช้เลย
-                            </Button>
-                        </Col>
-                        </Container>   
+                            <Col style={{"padding":"0"}} className="homeText text-center">อุปกรณ์เกษตกร
+                            </Col>
+                        </Container>
                     </Row>
                     <Row className="filler">
                     </Row>
+                </div>
+                <br/>
+                <br/>
+                <Container>
+                    <Row>
+                        <Col md={4}></Col>
+                        <Col>
+                            <Col sm={2}></Col>
+                            <Col sm={8}>                        <p>พวกเราเชื่อว่าชาวเกษตรสามารถซื้อถูกขายแพงได้ 
+
+ด้วยระบบไทม์แชร์ของเราคุณสามารถซื้ออุปกรรณ์ที่สำคัญสำหรับแค่ช่วงเวลาที่คุณไช้
+
+ซื้ออุปกรรณ์ในราคาต่ำกว่า 50% และหมดกังวลด้วยระบบประกัน 2 ปีของเรา 
+
+หมดการเถียงกันเรื่องอุปกรรณ์ด้วยระบบ Mr Share ตัวกลางของเรา รับสินค้า ได้สินค้าเหมือนเช่าราคาเหมือนซื้อ!</p>
+</Col>
+                            <Col sm={2}></Col>
+                        </Col>
+                    </Row>
                 </Container>
                 <br/>
                 <br/>
                 <Container>
-                    <h1>โครงการเราคืออะไร?</h1>
-                    <p>การกญดรำ่ร่ดไพนา าพนำา นพาำนๆพาำน พนาำ นๆพาน ำาๆนพ านำๆไพา นาไพน าๆำไนพา นๆาพนำาไๆ นพาำๆไนพ านำไๆาพ นำาๆนพๆ การกญดรำ่ร่ดไพนา าพนำา นพาำนๆพาำน พนาำ นๆพาน ำาๆนพ านำๆไพา นาไพน าๆำไนพา นๆาพนำาไๆ นพาำๆไนพ านำไๆาพ นำาๆนพๆการกญดรำ่ร่ดไพนา าพนำา นพาำนๆพาำน พนาำ นๆพาน ำาๆนพ านำๆไพา นาไพน าๆำไนพา นๆาพนำาไๆ นพาำๆไนพ านำไๆาพ นำาๆนพๆการกญดรำ่ร่ดไพนา าพนำา นพาำนๆพาำน พนาำ นๆพาน ำาๆนพ านำๆไพา นาไพน าๆำไนพา นๆาพนำาไๆ นพาำๆไนพ านำไๆาพ นำาๆนพๆการกญดรำ่ร่ดไพนา าพนำา นพาำนๆพาำน พนาำ นๆพาน ำาๆนพ านำๆไพา นาไพน าๆำไนพา นๆาพนำาไๆ นพาำๆไนพ านำไๆาพ นำาๆนพๆการกญดรำ่ร่ดไพนา าพนำา นพาำนๆพาำน พนาำ นๆพาน ำาๆนพ านำๆไพา นาไพน าๆำไนพา นๆาพนำาไๆ นพาำๆไนพ านำไๆาพ นำาๆนพๆ</p>
-                </Container>
-                <br/>
-                <br/>
-                <Container>
-                    <h1>สินค้าใหม่</h1>
+                    <h2 className="text-center">เช่าสินค้าคุณเลย</h2>
+                    <Row>
+                        <Col md={3} className="p-5"><img src={Graphics1} alt="React Logo" width="100%"/>
+                            <div className="text-center mt-4 info-font">สั่งสินค้าจากเวบ</div>
+                        </Col>
+                        <Col md={3} className="p-5"><img src={Graphics4} alt="React Logo" width="100%"/>
+                            <div className="text-center mt-4 info-font">จองเวลาไทม์แชร์</div>
+                        </Col>
+                        <Col md={3} className="p-5"><img src={Graphics3} alt="React Logo" width="100%"/>
+                            <div className="text-center mt-4 info-font">รอยืนยันและจ่าย LINE</div>
+                        </Col>
+                        <Col md={3} className="p-5"><img src={Graphics2} alt="React Logo" width="100%"/>
+                            <div className="text-center mt-4 info-font">ของส่งภายใน 2 อาทิตย์</div>
+                        </Col>
+                    </Row>
+                    
+                    <h2 className="text-center">สินค้าใหม่</h2>
                     <Row>
                         <Col>
-                            <Card style={{ width: '15rem' }}>
+                            <Card style={{ width: '100%' }}>
                                     <Card.Img variant="top" src={require("./Dir.jpeg")}/>
                                     <Card.Body>
-                                        <Card.Title>เครื่องไถ</Card.Title>
                                         <Card.Text>
-                                            AR-14730 <br/>
-                                            ยี่ห้อ: โปโล
+                                            เครื่องไถ
+                                            ยี่ห้อโปโล
                                             <div className="oldPrice">
                                                 THB7000
                                             </div>
@@ -54,67 +83,64 @@ export default class Home extends Component {
                                 </Card>                
                         </Col>
                         <Col>
-                        <Card style={{ width: '15rem' }} className="cust-card">
-                                <Card.Img className="img-item" variant="top" src={require("./Dir.jpeg")}/>
-                                <Card.Body>
-                                    <Card.Title>เครื่องไถ</Card.Title>
-                                    <Card.Text>
-                                        AR-14730 <br/>
-                                        ยี่ห้อ: โปโล
-                                        <div className="oldPrice">
-                                            THB7000
-                                        </div>
-                                        <span className="newPrice">
-                                            THB3000 &nbsp;
-                                        </span>
-                                        <span>
-                                            / 6  เดือน 3 ปี
-                                        </span>
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>                
+                            <Card style={{ width: '100%' }}>
+                                    <Card.Img variant="top" src={require("./Dir.jpeg")}/>
+                                    <Card.Body>
+                                        <Card.Text>
+                                            เครื่องไถ
+                                            ยี่ห้อโปโล
+                                            <div className="oldPrice">
+                                                THB7000
+                                            </div>
+                                            <span className="newPrice">
+                                                THB3000 &nbsp;
+                                            </span>
+                                            <span>
+                                                / 6  เดือน 3 ปี
+                                            </span>
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>                
                         </Col>
                         <Col>
-                            <Card style={{ width: '15rem' }}>
-                                <Card.Img variant="top" src={require("./Dir.jpeg")}/>
-                                <Card.Body>
-                                    <Card.Title>เครื่องไถ</Card.Title>
-                                    <Card.Text>
-                                        AR-14730 <br/>
-                                        ยี่ห้อ: โปโล
-                                        <div className="oldPrice">
-                                            THB7000
-                                        </div>
-                                        <span className="newPrice">
-                                            THB3000 &nbsp;
-                                        </span>
-                                        <span>
-                                            / 6  เดือน 3 ปี
-                                        </span>
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>                
+                            <Card style={{ width: '100%' }}>
+                                    <Card.Img variant="top" src={require("./Dir.jpeg")}/>
+                                    <Card.Body>
+                                        <Card.Text>
+                                            เครื่องไถ
+                                            ยี่ห้อโปโล
+                                            <div className="oldPrice">
+                                                THB7000
+                                            </div>
+                                            <span className="newPrice">
+                                                THB3000 &nbsp;
+                                            </span>
+                                            <span>
+                                                / 6  เดือน 3 ปี
+                                            </span>
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>                
                         </Col>
                         <Col>
-                            <Card style={{ width: '15rem' }}>
-                                <Card.Img variant="top" src={require("./Dir.jpeg")}/>
-                                <Card.Body>
-                                    <Card.Title>เครื่องไถ</Card.Title>
-                                    <Card.Text>
-                                        AR-14730 <br/>
-                                        ยี่ห้อ: โปโล
-                                        <div className="oldPrice">
-                                            THB7000
-                                        </div>
-                                        <span className="newPrice">
-                                            THB3000 &nbsp;
-                                        </span>
-                                        <span>
-                                            / 6  เดือน 3 ปี
-                                        </span>
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>                
+                            <Card style={{ width: '100%' }}>
+                                    <Card.Img variant="top" src={require("./Dir.jpeg")}/>
+                                    <Card.Body>
+                                        <Card.Text>
+                                            เครื่องไถ
+                                            ยี่ห้อโปโล
+                                            <div className="oldPrice">
+                                                THB7000
+                                            </div>
+                                            <span className="newPrice">
+                                                THB3000 &nbsp;
+                                            </span>
+                                            <span>
+                                                / 6  เดือน 3 ปี
+                                            </span>
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>                
                         </Col>
                     </Row>
                 </Container>
